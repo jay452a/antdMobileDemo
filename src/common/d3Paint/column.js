11d38be9647b45b872fb659f9a64be918687fda5
hao.zhang <hao.zhang@flaginfo.com.cn>
2018-01-08 17:58:02 +0800

    d3比例尺

--- src/common/d3Paint/column.js
+++ src/common/d3Paint/column.js
@@ -4,12 +4,13 @@
 const column = function (d3,dom,data) {
   var width = 400
   var height = 400
-  var svg = d3.select(dom).append('svg')
+  var svg = d3.select(dom)
   svg.attr('width', width).attr('height', height)
  
   var rectStep = 35
   var rectWidth = 30
-  svg.selectAll('rect').data(data).enter().append('rect').attr('fill','red')
+  var updateRect =  svg.selectAll('rect').data(data) // 填充数据
+  updateRect.enter().append('rect').attr('fill','red') // enter是数据多余，用append处理
     .attr('x', function (d,i) {
       return i*rectStep
     })
@@ -19,6 +20,46 @@ const column = function (d3,dom,data) {
     .attr('width',rectWidth)
     .attr('height', function (d,i) {
       return d
+    })
+  var updateText = svg.selectAll('text').data(data)
+  updateText.enter().append('text')
+    .attr('fill', 'blue')
+    .attr('font-size', '12px')
+    .attr('x',function (d,i) {
+      return i*rectStep
+    })
+    .attr('y',function (d,i) {
+      return height - d
+    })
+    .attr('dx',function (d,i) {
+      return rectWidth/2
+    })
+    .attr('dy','-2px')
+    .attr('text-anchor','middle')
+    .text(function (d) {
+      return d
+    })
+  // 线性比例尺
+  var linear = d3.scaleLinear() //线性比例尺新API
+    .domain([1,3])
+    .range([2,6])
+  console.log(linear(5))
+  // 量子比例尺和阈值比例尺差不多
+  var quantize = d3.scaleQuantile()
+    .domain([0,50])
+    .range(['#888','#666','#444','#222','#000'])
+  var r = [50,45,30,15,10]  // 定义圆的半径
+  var updateCircle = svg.selectAll('circle').data(r)
+  updateCircle.enter().append('circle')
+    .attr('cx', function (d, i) {
+      return 50 + i*30
+    })
+    .attr('cy',50)
+    .attr('r',function (d) {
+      return d
+    })
+    .attr('fill',function (d) {
+      return quantize(d)
     })
 }
 export default column
--- src/modules/view/d3Test.js
+++ src/modules/view/d3Test.js
@@ -7,28 +7,21 @@ var d3 = require('d3')
 export default class d3Test extends Component{
   constructor(props){
     super(props)
+    this.state = {
+      dataRect: [20,15,60,47,25,69]
+    }
+    this.addRectData = this.addRectData.bind(this)
+  }
+  addRectData () {
   }
   componentDidMount () {
-    var div = d3.selectAll('p')
-    var update = div.data([1,2,3]) // 填充数据
-    var enter = update.enter() // enter是数据多余，用append处理
-    var exit = update.exit()  //exit是div多余，用删除处理
-    console.log(enter, 1)
-    update.text((d) => {
-      return d
-    })
-    enter.append('p').html((d) => {
-      return d
-    })
-    exit.remove()
-    column(d3, '#svgArea',[20,15,60,47,25,69])
-    
+    column(d3, '#svgArea',this.state.dataRect)
   }
   render () {
     return (
       <div id="d3Container">
-        <p></p>
-        <div id="svgArea" style={{'textAlign':'center'}}></div>
+        <button onClick={this.addRectData}>add</button>
+        <svg id="svgArea" style={{'margin':'auto','display':'block'}}></svg>
       </div>
     )
   }
